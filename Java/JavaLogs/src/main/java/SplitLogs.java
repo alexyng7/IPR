357abe57b2626959d070501ee8795b1ba6298e7a
Alexey Gaybovich <a.gaybovich@pflb.ru>
2024-06-27 08:05:55 +0300

    чиним javalogs 2

---
+++ Java/JavaLogs/src/main/java/SplitLogs.java
@@ -0,0 +1,44 @@
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+
+public class SplitLogs {
+    public static void main(String[] args) throws IOException {
+
+        String fileLogPath = args[0]; // получение аргуемена путь до целевого файла
+        String filesLogPath = args[1]; // получение аргуемента путь сохранения новых файлов
+
+        File file = new File(fileLogPath);
+        long fileSize = file.length(); // получаем размер файла
+
+        int count_file = 5; // количество файлов логов
+
+        if (fileSize > 0) { // проверка пустой ли файл
+            long fileSizeTemp = fileSize;
+            try (BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(file), "windows-1251")))
+            { // считываем файл с учетом кодировки
+                for (int i = 1; i < (count_file + 1); i++) // создаем указанное количество фалов для записи
+                {
+                    if (fileSizeTemp == 0) break; // если файл весь вычитали, то новых файлов не создается
+                    String filesLogPath_n = filesLogPath + "_" + i + ".txt"; // задаем название и расширение файлов
+                    File file_n = new File(filesLogPath_n);
+                    file_n.createNewFile(); // создаем новый файл, если он не создан
+                    try (FileOutputStream fos = new FileOutputStream(file_n, false);
+                         BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(fos, StandardCharsets.UTF_8)))
+                    { // поток записи строк в файл с учетом кодировки
+                        long fileSize_n = file_n.length(); // Проверяем размер файла в, который пишем строки
+
+                        String line;
+                        while (fileSize_n < fileSize / (count_file) && (line = reader.readLine()) != null)
+                        { //проверяем размер файла в котоый пишем и естли не прочитанные строки в исходном файле
+                            writer.write(line + "\n"); // пишем строки в файл
+                            fileSize_n = file_n.length(); // обновляем вес нового файла
+                        }
+                        fileSizeTemp -= fileSize_n; // вычетаем записанный объем из общего объема
+                        writer.flush();
+                    }
+                }
+            }
+        }
+        else System.out.println("ВНИМАНИЕ! Указанный файл пустой."); // если файл пустой выдаст предупреждение
+    }
+}
